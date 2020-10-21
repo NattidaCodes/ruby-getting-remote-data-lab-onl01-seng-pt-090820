99e3900138b9c318c7d2589da93d071c1134c9f2
Nattida Wong <nattidac.wong@gmail.com>
2020-10-21 21:41:06 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,5 +6,9 @@ class GetRequester
   end
 
   def get_response_body
+    require 'open-uri'
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+uri = URI.parse(url)
+uri.open.string
   end
 end
